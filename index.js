3c172e79ee3884d07dcd102c737cec12bcaff480
bh1ff <ahmedayad1111@gmail.com>
2023-10-13 17:44:21 +0100

    index js new manager

--- index.js
+++ index.js
@@ -10,6 +10,69 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const tMembers = [];
+const idList = [];
+
+const appM = () => {
+  function newManager() {
+    console.log("Add a New Member");
+    inquirer.createPromptModule([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is your team manager's name",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          } else {
+            return "please enter a name";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "manageId",
+        message: "What is your team manager's id",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          } else {
+            return "please enter a valid id";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your team manager's email?",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          } else {
+            return "please enter a valid email";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "managerOffice",
+        message: "What is your team manager's office number",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          } else {
+            return "please enter a valid office number";
+          }
+        },
+      },
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.manageId, answers.managerEmail, answers.managerOffice)
+        tMembers.push(manager);
+        idList.push(answers.manageId)
+    })
+  }
+};
+
+appM();
